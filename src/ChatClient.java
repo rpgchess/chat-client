650ded82f0997ed48389c16c494ffa56be9d6c3c
Claudio A. Martins <rpgchess@gmail.com>
2019-11-06 00:33:11 -0300

    Project Chat Side Client

---
+++ src/ChatClient.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ChatClient {
+	public static void main(String[] args) throws UnknownHostException,	IOException	{
+		new	ChatClient("192.168.150.141",12345).executa();
+	}
+	private String host;
+	private int porta;
+	public ChatClient(String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente = new Socket(this.host,this.porta);
+		System.out.println("O cliente se conectou ao servidor!");
+		
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new	Thread(r).start();
+
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		while (teclado.hasNextLine())
+			saida.println(teclado.nextLine());
+		saida.close();
+		teclado.close();
+		cliente.close();								
+	}
+}
---
+++ src/Recebedor.java
@@ -0,0 +1,15 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Recebedor implements Runnable {
+	private InputStream servidor;
+	public Recebedor(InputStream servidor) {
+		this.servidor = servidor;
+	}
+	public void run() {
+		Scanner s = new Scanner(this.servidor); 
+		while (s.hasNextLine())
+			System.out.println(s.nextLine());
+		s.close();
+	}
+}
